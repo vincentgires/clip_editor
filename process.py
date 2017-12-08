a6252ec65f860c13c271ff353a01b6a6ffae486c
Vincent Girès <mail@vincentgires.com>
2017-12-08 11:28:20 +0100

    support of custom image files

--- process.py
+++ process.py
@@ -23,29 +23,55 @@ def update_frame(scene):
 
 bpy.app.handlers.frame_change_pre.append(update_frame)
 
-filename = os.path.basename(settings['sequences'][0]['path'])
 
 # MOVIECLIP
 settings['sequences'][0]['path'] = os.path.normpath(settings['sequences'][0]['path'])
 if sys.platform.startswith('linux'):
     settings['sequences'][0]['path'] = settings['sequences'][0]['path'].replace('//', '/')
 
-clip = movieclips.load(settings['sequences'][0]['path'])
-x, y = clip.size
+path = settings['sequences'][0]['path']
+files = settings['sequences'][0]['files']
+
+# MOVIECLIP
+if files:
+    movieclip = movieclips.load(os.path.join(path, files[0]))
+else:
+    movieclip = movieclips.load(path)
+x_res, y_res = movieclip.size
 
 # SEQUENCER
 if not scene.sequence_editor:
     scene.sequence_editor_create()
-
 sequences = scene.sequence_editor.sequences
 
-frame_start = int(filename.split('.')[-2])
-sequence_strip = sequences.new_clip(
-    name='sequence_clip',
-    clip=clip,
-    channel=1,
-    frame_start=frame_start
-    )
+channel = 1
+
+if os.path.isfile(path):
+    filename = os.path.basename(settings['sequences'][0]['path'])
+    frame_start = int(filename.split('.')[-2])
+    sequence_strip = sequences.new_clip(
+        name='sequence_clip',
+        clip=movieclip,
+        channel=channel,
+        frame_start=frame_start
+        )
+
+elif os.path.isdir(path):
+    files = settings['sequences'][0]['files']
+    filename = os.path.basename(files[0])
+    frame_start = int(filename.split('.')[-2])
+    
+    first_frame = os.path.join(path, files[0])
+    sequence_strip = sequences.new_image(
+        name='image_sequence',
+        filepath=first_frame,
+        channel=channel,
+        frame_start=frame_start
+        )
+
+    for file in files[1:]:
+        name = os.path.basename(file)
+        sequence_strip.elements.append(name)
 
 # SET OVERLAY
 if settings['overlays']:
@@ -55,7 +81,7 @@ if settings['overlays']:
         channel=2,
         frame_start=frame_start
         )
-    overlay_scene.frame_final_duration = clip.frame_duration
+    overlay_scene.frame_final_duration = movieclip.frame_duration
     overlay_scene.blend_type = 'ALPHA_OVER'
     
     for overlay in settings['overlays']:
@@ -74,10 +100,10 @@ if settings['overlays']:
         
         
 # RENDER SETTINGS
-scene.frame_end = frame_start+clip.frame_duration-1
+scene.frame_end = frame_start+movieclip.frame_duration-1
 scene.frame_start = frame_start
-scene.render.resolution_x = x
-scene.render.resolution_y = y
+scene.render.resolution_x = x_res
+scene.render.resolution_y = y_res
 scene.render.resolution_percentage = 100
 scene.render.filepath = settings['output']
 

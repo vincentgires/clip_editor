c915c5e4a6080fa31394e9c178bb2ac01a08c872
Vincent Girès <mail@vincentgires.com>
2018-09-12 17:22:57 +0200

    Using binaries from PATH env

--- clip_editor/config.py
+++ clip_editor/config.py
@@ -8,8 +8,8 @@ TEMPLATE_PATH = os.path.join(current_dir, 'template.blend')
 SCRIPT_PATH = os.path.join(current_dir, 'process.py')
 
 if sys.platform.startswith('linux'):
-    BLENDER_BIN = '/nwave/software/Blender/2.79a/linux64/blender'
-    FFMPEG_BIN = '/nwave/software/FFmpeg/4.0/linux64/ffmpeg'
+    BLENDER_BIN = 'blender'
+    FFMPEG_BIN = 'ffmpeg'
 else:
-    BLENDER_BIN = '//nwave/software/Blender/2.79a/win64/blender.exe'
-    FFMPEG_BIN = '//nwave/software/FFmpeg/4.0/win64/bin/ffmpeg.exe'
+    BLENDER_BIN = 'blender.exe'
+    FFMPEG_BIN = 'ffmpeg.exe'

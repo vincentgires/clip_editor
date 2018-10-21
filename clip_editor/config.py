49c8bbc5a6525c001c9b72cf2fd25870275dc8c1
Vincent Girès <mail@vincentgires.com>
2018-10-22 00:29:09 +0200

    Move files

--- clip_editor/config.py
+++ clip_editor/config.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-current_dir = os.path.dirname(__file__)
-current_dir = os.path.normpath(current_dir)
+_current_dir = os.path.dirname(__file__)
+_current_dir = os.path.normpath(_current_dir)
 
-SCRIPTS_PATH = os.path.join(current_dir, 'scripts')
-TEMPLATE_PATH = os.path.join(SCRIPTS_PATH, 'template.blend')
-PROCESS_SCRIPT_PATH = os.path.join(SCRIPTS_PATH, 'process.py')
-EXTRACT_SCRIPT_PATH = os.path.join(SCRIPTS_PATH, 'extract.py')
+BLENDER_FOLDER_PATH = os.path.join(_current_dir, 'blender')
+BLENDER_SCRIPTS_PATH = os.path.join(BLENDER_FOLDER_PATH, 'scripts')
+TEMPLATE_PATH = os.path.join(BLENDER_FOLDER_PATH, 'template.blend')
+PROCESS_SCRIPT_PATH = os.path.join(BLENDER_SCRIPTS_PATH, 'process.py')
+EXTRACT_SCRIPT_PATH = os.path.join(BLENDER_SCRIPTS_PATH, 'extract.py')
 
 if sys.platform.startswith('linux'):
     BLENDER_BIN = 'blender'
